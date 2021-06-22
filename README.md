1d9bedbf7e3ce1516be76d7d08641366cd4836d3
krchopra <krishan.chopra@oracle.com>
2021-06-22 12:47:33 +0530

    retrofitted test case to use asyn await

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ this repo contains my workbook for practicing smart contracts
 
 1.) First Smart Contract :- inbox 
 this smart contract is the first smart contract for practice . 
+
+
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,15 +4,16 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
     // beforeEach runs before running each test
     // Get a list of all accounts
-
-    web3.eth.getAccounts().then(fetchedAccounts => {
-       console.log(fetchedAccounts);
-    });
+    accounts = await web3.eth.getAccounts();
 });
 
 describe('Inbox' , () => {
-    it('deploys a contract', () => {});
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
 });
